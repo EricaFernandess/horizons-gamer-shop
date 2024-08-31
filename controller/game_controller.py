0aa33a5b65be9fdd6270c620221e3d4b7a7ae9f7
erica <ericafernandeslive10@gmail.com>
2024-08-31 20:22:17 -0300

    Criando controller do micro serviço.

---
+++ controller/game_controller.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, request, jsonify
+from service.game_service import GameService
+
+game_controller = Blueprint('game_controller', __name__)
+game_service = GameService()
+
+@game_controller.route('/register-game', methods=['POST'])
+def register_game():
+    data = request.get_json()
+
+    game_service.register_game(
+        name=data['name'],
+        photo=data['photo'],
+        price=data['price'],
+        genre=data['genre'],
+        launch_date=data['launch_date'],
+        platform=data['platform']
+    )
+
+    response = {
+        'message': 'Dados recebidos e salvos com sucesso!',
+        'received_data': data
+    }
+
+    return jsonify(response), 201
+
+@game_controller.route('/games', methods=['GET'])
+def get_games():
+    games = game_service.fetch_all_games()
+    return jsonify(games), 200
